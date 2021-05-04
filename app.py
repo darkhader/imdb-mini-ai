472419afab283d2264b7e774e1105efa869ef365
Nguyễn Hoàng Hiệp <dr.hiep98@gmail.com>
2021-05-04 22:24:40 +0700

    on E

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 from flask import Flask, request
+from flask_cors import CORS, cross_origin
 from app_service import AppService
 import json
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 appService = AppService();
 
 
 @app.route('/')
+@cross_origin(supports_credentials=True)
 def home():
     return "App Works!!!"
 
 @app.route('/api/<string:title>')
+@cross_origin(supports_credentials=True)
 def get(title):
     return appService.get(title)
 
